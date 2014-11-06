9a8a1832ef7b86b127ba63f587b0230d8d050a8b
Kevin Bretney <kevinbretney@gmail.com>
2014-11-05 17:06:31 -0800

    Added makefiles for mavstation and updated upload.mk to support mavstation

---
+++ makefiles/config_mavstation_default.mk
@@ -0,0 +1,11 @@
+#
+# Makefile for the mavstation_default configuration
+#
+
+#
+# Board support modules
+#
+MODULES		+= drivers/stm32
+MODULES		+= drivers/boards/mavstation
+MODULES		+= modules/mavstation_firmware
+#MODULES		+= modules/px4_simple_app
