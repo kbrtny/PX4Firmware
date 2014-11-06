2f4930559a676973d58187330ae1e3d9660ce3ee
Kevin Bretney <kevinbretney@gmail.com>
2014-11-06 12:32:47 -0800

    added mavstation Image prototype.  Runs on the latest version.

--- makefiles/upload.mk
+++ makefiles/upload.mk
@@ -34,6 +34,10 @@ upload-serial-px4fmu-v2:	$(BUNDLE) $(UPLOADER)
 upload-serial-aerocore:
 	openocd -f $(PX4_BASE)/makefiles/gumstix-aerocore.cfg -c 'init; reset halt; flash write_image erase $(PX4_BASE)/../Bootloader/px4aerocore_bl.bin 0x08000000; flash write_image erase $(PX4_BASE)/Build/aerocore_default.build/firmware.bin 0x08004000; reset run; exit'
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> added mavstation Image prototype.  Runs on the latest version.
 upload-serial-mavstation:    $(BUNDLE) $(UPLOADER)
 	$(Q) $(PYTHON) -u $(UPLOADER) --port $(SERIAL_PORTS) $(BUNDLE)
 
