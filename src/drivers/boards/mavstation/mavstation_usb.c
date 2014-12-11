653f174f1134bb1ff2e261306292ec4997dc4ca5
Kevin Bretney <kevinbretney@gmail.com>
2014-12-11 11:02:03 -0800

    Board shows up on OS X but can't enumerate

--- src/drivers/boards/mavstation/mavstation_usb.c
+++ src/drivers/boards/mavstation/mavstation_usb.c
@@ -78,6 +78,7 @@ __EXPORT void stm32_usbinitialize(void)
 
   /* USB Soft Connect Pullup */
   stm32_configgpio(GPIO_USB_PULLUP);
+  stm32_gpiowrite(GPIO_USB_PULLUP, true);
 }
 
 /************************************************************************************
