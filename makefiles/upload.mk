7387e7bc4fd2b30276063640439f3453abf8c210
Kevin Bretney <kevinbretney@gmail.com>
2015-01-27 13:19:16 -0800

    merging

--- makefiles/upload.mk
+++ makefiles/upload.mk
@@ -36,6 +36,9 @@ upload-serial-aerocore:
 upload-serial-mavstation:    $(BUNDLE) $(UPLOADER)
 	$(Q) $(PYTHON) -u $(UPLOADER) --port $(SERIAL_PORTS) $(BUNDLE)
 
+upload-serial-mavstation_default:    $(BUNDLE) $(UPLOADER)
+	$(Q) $(PYTHON) -u $(UPLOADER) --port $(SERIAL_PORTS) $(BUNDLE)
+
 
 #
 # JTAG firmware uploading with OpenOCD
